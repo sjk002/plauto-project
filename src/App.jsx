875c4bb7a6e3fa5518c345c7259ed14380a5f173
Shawn Kim <sjk002@ucsd.edu>
2023-05-19 14:20:21 -0700

    pushing analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import './styles/main.scss'
 import Home from './pages/Home'
+import ReactGA from 'react-ga'
+import { useEffect } from 'react'
 
 function App() {
+
+  useEffect(() => {
+    const TRACKING_ID = 'UA-268817949-1';
+    ReactGA.initialize(TRACKING_ID);
+
+    ReactGA.pageview('/')
+  }, [])
 
   return (
     <>
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import ReactGA from 'react-ga'
-
-const TRACKING_ID = 'UA-268817949-1';
-
-ReactGA.initialize(TRACKING_ID);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
