0169abe34beb767d77830219354d2b6359d401e4
Shawn Kim <sjk002@ucsd.edu>
2023-05-19 14:25:12 -0700

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import './styles/main.scss'
 import Home from './pages/Home'
-import ReactGA from 'react-ga'
+import ReactGA4 from 'react-ga4';
 import { useEffect } from 'react'
 
 function App() {
 
   useEffect(() => {
-    const TRACKING_ID = 'UA-268817949-1';
-    ReactGA.initialize(TRACKING_ID);
 
-    ReactGA.pageview('/')
+    const MEASUREMENT_ID = 'G-QZ64B65MGY';
+
+    ReactGA4.initialize(MEASUREMENT_ID);
+
+    ReactGA4.pageview('/')
   }, [])
 
   return (
