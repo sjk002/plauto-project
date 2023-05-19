6c964898f78e759c6c1bc633e0cda240fc26ed74
Shawn Kim <sjk002@ucsd.edu>
2023-05-18 18:05:12 -0700

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+  gtmId: 'GTM-8WC1W08RGF'
+}
+
+TagManager.initialize(tagManagerArgs)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
